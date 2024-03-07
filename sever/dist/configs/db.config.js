510cf68542102bf9e1fc218e12eb31049a18a791
binh0925028208 <you@example.com>
2024-03-07 15:50:54 +0700

    bosung

--- sever/dist/configs/db.config.js
+++ sever/dist/configs/db.config.js
@@ -6,8 +6,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
-const sequelize = new sequelize_1.Sequelize("Project", "root", String(process.env.PASSWORD), {
-    host: "localhost",
+const sequelize = new sequelize_1.Sequelize("bdglcwdet2wc48k4y5nz", "ut2eepzbd3wpsifw", "Pm6XoZO7CMEr6HZhdoOw", {
+    host: "bdglcwdet2wc48k4y5nz-mysql.services.clever-cloud.com",
     dialect: "mysql",
 });
 exports.default = sequelize;
--- sever/src/configs/db.config.ts
+++ sever/src/configs/db.config.ts
@@ -3,11 +3,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const sequelize = new Sequelize(
-  "Project",
-  "root",
-  String(process.env.PASSWORD),
+  "bdglcwdet2wc48k4y5nz",
+  "ut2eepzbd3wpsifw",
+  "Pm6XoZO7CMEr6HZhdoOw",
   {
-    host: "localhost",
+    host: "bdglcwdet2wc48k4y5nz-mysql.services.clever-cloud.com",
     dialect: "mysql",
   }
 );
