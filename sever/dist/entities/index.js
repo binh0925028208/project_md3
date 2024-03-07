e08d030717bbf7ac9b081c4df36b93f636f0ebf6
binh0925028208 <you@example.com>
2024-03-07 15:53:06 +0700

    bosung

--- sever/dist/entities/index.js
+++ sever/dist/entities/index.js
@@ -1,23 +1,31 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const comment_entity_1 = __importDefault(require("./comment.entity"));
+const orderItems_entity_1 = __importDefault(require("./orderItems.entity"));
+const product_entity_1 = __importDefault(require("./product.entity"));
+const ratting_entity_1 = __importDefault(require("./ratting.entity"));
+const user_entity_1 = __importDefault(require("./user.entity"));
 const createTable = () => {
-    // User.sync().then(() => {
-    //   console.log("User created");
-    // });
-    // Product.sync().then(() => {
-    //   console.log("Product created");
-    // });
-    // Ratting.sync().then(() => {
-    //   console.log("Ratting created");
-    // });
-    // Comment.sync().then(() => {
-    //   console.log("Comment created");
-    // });
-    // OrderItems.sync().then(() => {
-    //   console.log("orderItems created");
-    // });
-    // OrderItems.sync().then(() => {
-    //   console.log("OrderItems created");
-    // });
+    user_entity_1.default.sync().then(() => {
+        console.log("User created");
+    });
+    product_entity_1.default.sync().then(() => {
+        console.log("Product created");
+    });
+    ratting_entity_1.default.sync().then(() => {
+        console.log("Ratting created");
+    });
+    comment_entity_1.default.sync().then(() => {
+        console.log("Comment created");
+    });
+    orderItems_entity_1.default.sync().then(() => {
+        console.log("orderItems created");
+    });
+    orderItems_entity_1.default.sync().then(() => {
+        console.log("OrderItems created");
+    });
 };
 exports.default = createTable;
--- sever/src/entities/index.ts
+++ sever/src/entities/index.ts
@@ -5,24 +5,24 @@ import Ratting from "./ratting.entity";
 import User from "./user.entity";
 
 const createTable = () => {
-  // User.sync().then(() => {
-  //   console.log("User created");
-  // });
-  // Product.sync().then(() => {
-  //   console.log("Product created");
-  // });
-  // Ratting.sync().then(() => {
-  //   console.log("Ratting created");
-  // });
-  // Comment.sync().then(() => {
-  //   console.log("Comment created");
-  // });
-  // OrderItems.sync().then(() => {
-  //   console.log("orderItems created");
-  // });
-  // OrderItems.sync().then(() => {
-  //   console.log("OrderItems created");
-  // });
+  User.sync().then(() => {
+    console.log("User created");
+  });
+  Product.sync().then(() => {
+    console.log("Product created");
+  });
+  Ratting.sync().then(() => {
+    console.log("Ratting created");
+  });
+  Comment.sync().then(() => {
+    console.log("Comment created");
+  });
+  OrderItems.sync().then(() => {
+    console.log("orderItems created");
+  });
+  OrderItems.sync().then(() => {
+    console.log("OrderItems created");
+  });
 };
 
 export default createTable;
